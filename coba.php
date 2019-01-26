5522c3621ddebcbb578a75b43d51bbfd814a4b5c
Brian <brian.rofiq@gmail.com>
2019-01-26 20:29:47 +0700

    sendgrid

--- coba.php
+++ coba.php
@@ -5,3 +5,29 @@
  * Date: 25/01/19
  * Time: 13:05
  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './PHPMailer-master/src/Exception.php';
+require './PHPMailer-master/src/PHPMailer.php';
+require './PHPMailer-master/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "brilliant.djaka21@gmail.com";
+$mail->Password = "brianrofiq123";
+$mail->setFrom('brilliant.djaka21@gmail.com', 'First Last');
+$mail->addAddress('brian.rofiq@gmail.com', 'John Doe');
+$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->msgHTML('<p>Tefa Jaya</p>');
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+
+}
--- forgot.php
+++ forgot.php
@@ -14,18 +14,21 @@ $db           = 'log';
 // Create connection
 $conn      = new mysqli($servername, $username, $password, $db);
 // Check connection
+$times = date('d-m-Y');
+    require('routeros_api.php');
+    require 'sendgrid-php.php';
 if ($conn->connect_error) {
-  echo "<script>var reload = prompt('Koneksi Error : $conn->connect_error . Tekan Ok Untuk Reload');
+  echo "<script>var reload = confirm('Koneksi Error : $conn->connect_error . Tekan Ok Untuk Reload');
 
     if (reload != null) {
         window.location.href = '$alamatReset';
     }</script>";
 } else {
-  echo "Connected successfully";
-  $sql    = "SELECT * FROM `limit` WHERE user= '$emailUser'";
+  //echo "Connected successfully";
+  $sql    = "SELECT * FROM `limit` WHERE user= '$emailUser' and tanggal = '$times' ";
   $result = $conn->query($sql);
   if ($result->num_rows > 0) {
-    echo "<script>var reload = prompt('Gagal Update Password . Tekan Ok Untuk Reload');
+    echo "<script>var reload = confirm('Maaf, anda baru mereset password anda hari ini. Silahkan cek Email . Tekan Ok Untuk Reload');
 
     if (reload != null) {
         window.location.href = '$alamatReset';
@@ -33,13 +36,12 @@ if ($conn->connect_error) {
   } else {
     $cmmnd = "=password=";
     $cmmnd .= $new_password;
-    require('routeros_api.php');
-    require 'sendgrid-php.php';
+    
     $newPass = $new_password;
-    echo 'Password Anda ' . $newPass . '     ';
+    //echo 'Password Anda ' . $newPass . '     ';
     $API        = new routeros_api();
     $API->debug = false;
-    if ($API->connect('192.168.43.18', 'admin', 'admin')) { //Silahkan di edit untuk user router mikrotik
+    if ($API->connect('192.168.43.17', 'admin', 'admin')) { //Silahkan di edit untuk user router mikrotik
       $API->write('/ip/hotspot/user/getall', false);
       $API->write('?name=' . $old_name);
       $READ  = $API->read(false);
@@ -50,42 +52,54 @@ if ($conn->connect_error) {
       $API->write($cmmnd);
       $READ  = $API->read(false);
       $ARRAY = $API->parse_response($READ);
-      echo '<script>alert("Password on server 1 changed successfully!");</script>';
+      
       //SendGrid
       $email = new \SendGrid\Mail\Mail();
-      $email->setFrom("tefa@gmail.com", "Wibi");
-      $email->setSubject("Sending with SendGrid is Fun");
-      $email->addTo("brian.rofiq@gmail.com", "Brian");
-      $email->addContent("text/html", "<html><p>Password Anda Adalah : " . $newPass . "</p> <strong>tefa loves you</strong></html>");
-      $sendgrid = new \SendGrid('SG.8hXB0F5XTK2hAS7evE3qSA.0JN-CETYD2y8cADjqxvmhmPYqk0Fdlq8HAYSXB3I7qA');
+      $email->setFrom("sarpra@smktelkom-mlg.sch.id", "sarpra");
+      $email->setSubject("RESET PASSWORD");
+      $email->addTo($emailUser, $emailUser);
+      $email->addContent("text/html", "<html><p>RESET USER PASSWORD INTERNER SMK TELKOM MALANG BERHASIL.<br>Password Anda Adalah : <b>" . $newPass . "</b>.<br> Terimakasih<br>Sarpra.</p></html>");
+      $sendgrid = new \SendGrid('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');
       try {
         $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
+       // print $response->statusCode() . "\n";
+        //print_r($response->headers());
+        //print $response->body() . "\n";
         //SQL
-        $sql = "INSERT INTO `limit`(`user`) VALUES ('$emailUser')";
-        if ($conn->query($sql) === TRUE) {
-          //echo "New record created successfully";
-        } else {
-          // echo "Error: " . $sql . "<br>" . $conn->error;
+
+        $sql = "INSERT INTO `limit`(`user`,`tanggal`) VALUES ('$emailUser','$times')";
+        try{
+            $conn->query($sql);
+            // echo "<script>
+            //         var reload = confirm('Password anda telah di reset dan dikirim ke email anda. Password Anda : $newPass . Tekan Ok Untuk Reload') if (reload != null) {window.location.href = '$alamatReset';}</script>";
+            echo "
+                <script>
+                 var reload = confirm('Berhasil. Password anda : $newPass . Tekan Ok Untuk Reload');
+                 if(reload != null) {
+                   window.location.href = '$alamatReset';
+                  }
+                </script>";
+
+        }catch (Exception $e){
+            echo "<script>var reload = confirm('Tidak bisa mengirim email. Password anda : $newPass . Tekan Ok Untuk Reload') if (reload != null) {window.location.href = '$alamatReset';}</script>";
+
         }
+
         $conn->close();
       }
       catch (Exception $e) {
-        echo "<script>var reload = prompt('Tidak bisa mengirim email Password anda $newPass . Tekan Ok Untuk Reload');
-
-    if (reload != null) {
-        window.location.href = '$alamatReset';
-    }</script>";
+        echo "<script>var reload = confirm('Tidak bisa mengirim email Password anda $newPass . Tekan Ok Untuk Reload');
+        if (reload != null) {window.location.href = '$alamatReset';}</script>";
       }
       $API->disconnect();
     } else {
-      echo "<script>var reload = prompt('Tidak bisa login ke router . Tekan Ok Untuk Reload');
 
-    if (reload != null) {
-        window.location.href = '$alamatReset';
-    }</script>";
+      echo "<script>
+           var reload = confirm('Tidak bisa login ke router . Tekan Ok Untuk Reload');
+            if(reload != null) {
+                window.location.href = '$alamatReset';
+            }</script>
+            ";
     }
   }
 }
